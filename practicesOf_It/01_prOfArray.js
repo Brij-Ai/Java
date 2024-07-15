96e37261f26efa899948b4eac06ccc21fda57956
Brij-Ai <Brijk0625@gmail.com>
2024-07-15 21:38:59 +0530

    new practice file added

---
+++ practicesOf_It/01_prOfArray.js
@@ -0,0 +1,8 @@
+// creating a Array variable
+const Car = ['Bmw','valvo','lambo','MClearn']
+console.log(Car[2])
+
+const myCar = Array('newCar')
+myCar[1] = 'hello'
+console.log(myCar.length)
+console.log(myCar)
