37c9e500ddf00b91aeca0cdd576f22060fec0a1d
Brij-Ai <Brijk0625@gmail.com>
2024-07-15 16:30:38 +0530

    little changes

--- 01_object.js
+++ 01_object.js
@@ -9,7 +9,7 @@ const mySym = Symbol('key1') //symbol declartion
 const JsUser = {
     name:"brij",
     age:18,
-    [mySym]:"my kEy 01",
+    [mySym]:"mykEy01",
     loction:"up",
     email:"brijk@gmail.com",
     lastLogIndays:['monday','sunday'],
@@ -30,7 +30,7 @@ console.log("after changes: ",JsUser.email)
 
 // creating a function of abject
     JsUser.greatin = function(){
-        console.log(`welcome ! , ${this["full name"]}`)
+        console.log(`welcome !,${this["full name"]}`)
     }
 console.log(JsUser.greatin())
 
