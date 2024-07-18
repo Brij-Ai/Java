b19e1b2469b619735f45dc43cf77763f0240deea
Brij-Ai <Brijk0625@gmail.com>
2024-07-18 09:50:03 +0530

    Introduction of for loop

---
+++ iteration/01_fforLoop.js
@@ -0,0 +1,23 @@
+// for looop
+// ctr + d shortcut
+for( let i = 0; i < 10; i++){
+    //const element = i
+    if(element == 4){
+       // console.log("welcome");
+    }
+   // console.log(element);
+}
+
+for(let i = 0; i<=10; i++){
+   // console.log(`Outer ${i}`);
+    for(let j = 0; j <=10; j++){
+        
+        // console.log(`inner ${j}`);
+    }
+}
+
+// for copy duplicate alt+shift+down key
+
+// break and continue keyWord
+
+
